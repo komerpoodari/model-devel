131debcde808a7ee0bcecba0fbcbd5405f128eb6
Komuraiah Poodari <poodari@yahoo.com>
2018-11-27 00:46:30 -0800

    minor changes & clean-up offline

--- sim_model_train.py
+++ sim_model_train.py
@@ -1,10 +1,12 @@
 import os
+import glob
+import csv
+import random
+
 import numpy as np
 import pandas as pd
-import random
 import cv2
-import csv
-import glob
+
 from sklearn import model_selection
 
 from keras import backend as K
@@ -19,12 +21,9 @@ ROOT_PATH = './'
 BATCH_SIZE = 4
 EPOCHS = 15
 
-IMAGE_HEIGHT = 600
-IMAGE_WIDTH = 800
-IMAGE_CHANNEL = 3
-
-TOP_CROP = 266
-BOTTOM_CROP = 326
+IMG_H = 600
+IMG_W = 800
+IMG_CHN = 3
 
 MODEL_FILE_NAME = './sim_model.h5'
 
@@ -32,114 +31,108 @@ MODEL_FILE_NAME = './sim_model.h5'
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
-# if not os.path.exists('./traffic_light.csv'):
-#     with open('traffic_light.csv', 'w', newline='') as csvfile:
-#         mywriter = csv.writer(csvfile)
-#         mywriter.writerow(['path', 'class', 'color'])
-
-#         for myclass, directory in enumerate(['nolight', 'red', 'yellow', 'green']):
-#             for filename in glob.glob('./data/real_training_data/{}/*.jpg'.format(directory)):
-#                 filename = '/'.join(filename.split('\\'))
-#                 mywriter.writerow([filename, myclass, directory])
-#     print('CSV file created successfully')
-# else:
-#     print('CSV already present')
-
-
+# Handy Augment function to apply random brightness to make model robust
 def random_brightness(image):
     # Convert 2 HSV colorspace from RGB colorspace
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # Generate new random brightness
-    rand = random.uniform(0.3, 1.0)
+    rand = random.uniform(0.4, 1.0)
     hsv[:, :, 2] = rand*hsv[:, :, 2]
     # Convert back to RGB colorspace
     new_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return new_img
 
 
+# Handy Augment function to zoom
 def zoom(image):
     zoom_pix = random.randint(0, 10)
     zoom_factor = 1 + (2*zoom_pix)/IMAGE_HEIGHT
-    image = cv2.resize(image, None, fx=zoom_factor,
-                       fy=zoom_factor, interpolation=cv2.INTER_LINEAR)
-    top_crop = (image.shape[0] - IMAGE_HEIGHT)//2
-#     bottom_crop = image.shape[0] - top_crop - IMAGE_HEIGHT
-    left_crop = (image.shape[1] - IMAGE_WIDTH)//2
-#     right_crop = image.shape[1] - left_crop - IMAGE_WIDTH
-    image = image[top_crop: top_crop+IMAGE_HEIGHT,
-                  left_crop: left_crop+IMAGE_WIDTH]
-    return image
+    image = cv2.resize(image, None, fx=zoom_factor,fy=zoom_factor, interpolation=cv2.INTER_LINEAR)
+    top_crop = (image.shape[0] - IMG_H)//2
+    left_crop = (image.shape[1] - IMG_W)//2
+    new_img = image[top_crop: top_crop+IMG_H, left_crop: left_crop+IMG_W]
+    return new_img
 
 
 # fuction to read image from file
-def get_image(index, data, should_augment):
+def get_image(index, data, apply_augment=False):
     # Read image and appropiately traffic light color
     image = cv2.imread(os.path.join(
         ROOT_PATH, data['path'].values[index].strip()))
     color = data['class'].values[index]
-    lucky = random.randint(0, 1)
-    too_lucky = random.randint(0, 1)
-    unlucky = random.randint(0, 1)
-    if should_augment:
-        if lucky == 1:
+
+    if apply_augment:
+        apply_brightness = random.randint(0, 1)
+        apply_flip = random.randint(0, 1)
+        apply_zoom = random.randint(0, 1)
+        
+        if apply_brightness == 1:
             image = random_brightness(image)
-        if too_lucky == 1:
+        if apply_flip == 1:
             image = cv2.flip(image, 1)
-        if not unlucky == 1:
+        if apply_zoom == 1:
             image = zoom(image)
 
     return [image, color]
 
+
 # generator function to return images batchwise
-
-
-def generator(data, should_augment=False):
+def generator(data_set, apply_augment=False):
     while True:
-        # Randomize the indices to make an array
-        indices_arr = np.random.permutation(data.count()[0])
-        for batch in range(0, len(indices_arr), BATCH_SIZE):
+        # Shuffle the data set
+        shuffled_indices = np.random.permutation(data_set.count()[0])
+        for batch in range(0, len(shuffled_indices), BATCH_SIZE):
             # slice out the current batch according to batch-size
-            current_batch = indices_arr[batch:(batch + BATCH_SIZE)]
+            current_batch = shuffled_indices[batch:(batch + BATCH_SIZE)]
 
             # initializing the arrays, x_train and y_train
-            x_train = np.empty(
-                [0, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL], dtype=np.float32)
+            x_train = np.empty([0, IMG_H, IMG_W, IMG_CHN], dtype=np.float32)
             y_train = np.empty([0], dtype=np.int32)
 
             for i in current_batch:
                 # get an image and its corresponding color for an traffic light
-                [image, color] = get_image(i, data, should_augment)
+                [image, color] = get_image(i, data_set, apply_augment)
 
                 # Appending them to existing batch
                 x_train = np.append(x_train, [image], axis=0)
                 y_train = np.append(y_train, [color])
-            y_train = to_categorical(y_train, num_classes=4)
+            y_train = to_categorical(y_train, num_classes=NUM_CLASSES)
 
             yield (x_train, y_train)
 
-
-def get_model(time_len=1):
-
+#Define the model
+def construct_model():
     model = Sequential()
-    model.add(Cropping2D(cropping=((0, 0), (0, 0)),
-                         input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL)))
+    
+    #Define Input layer
+    #Add bottom cropping until bottom 180 the car bonnet is visible, mostly
+    model.add(Cropping2D(cropping=((0, BOTTOM_CROP), (0, 0)),
+                         input_shape=(IMG_H, IMG_W, IMG_CHN)))
+    #Normalize the pixel values
     model.add(Lambda(lambda x: x/127.5 - 1.))
+    
+    #Define First Layer
     model.add(Conv2D(32, 8, strides=(4, 4), padding="same", activation='relu'))
+    #Follow with Max Pooling Layer
     model.add(MaxPooling2D(2, 2))
-    model.add(Conv2D(32, 4, strides=(2, 2), padding="same", activation='relu'))
+    
+    # Define second layer & a max pooling follow-up layer
+    model.add(Conv2D(64, 4, strides=(2, 2), padding="same", activation='relu'))
     model.add(MaxPooling2D(2, 2))
-    # model.add(Conv2D(64, 5, strides=(2, 2), padding="same", activation='relu'))
+    
+    #Time to flatten add dropout to minimize overfitting
     model.add(Flatten())
-    model.add(Dropout(.35))
+    model.add(Dropout(.4))
+    
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(.5))
-    model.add(Dense(4))
-    # model.add(Lambda(lambda x: (K.exp(x) + 1e-4) / (K.sum(K.exp(x)) + 1e-4)))
+    
+    model.add(Dense(NUM_CLASSES))
+ 
     model.add(Lambda(lambda x: K.tf.nn.softmax(x)))
     
-
-    model.compile(optimizer=Adam(lr=1e-4),
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+    # Think about learning rate later, Use Adam 
+    model.compile(optimizer=Adam(lr=5e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
     model.summary()
 
@@ -148,35 +141,28 @@ def get_model(time_len=1):
 
 if __name__ == "__main__":
 
-    data = pd.read_csv(os.path.join('./sim_train.csv'))
+    data_set = pd.read_csv(os.path.join('./sim_train.csv'))
 
     # Split data into random training and validation sets
-    d_train, d_valid = model_selection.train_test_split(data, test_size=.2)
+    x_train, x_valid = model_selection.train_test_split(data_set, test_size=.2)
 
-    train_gen = generator(d_train, False)
-    validation_gen = generator(d_valid, False)
+    train_gen = generator(x_train, False)
+    validation_gen = generator(x_valid, False)
 
-    model = get_model()
+    model = construct_model()
 
     # checkpoint to save best weights after each epoch based on the improvement in val_loss
     checkpoint = ModelCheckpoint(MODEL_FILE_NAME, monitor='val_loss', verbose=1,save_best_only=True, mode='min',save_weights_only=False)
-    callbacks_list = [checkpoint] #,callback_each_epoch]
+    callbacks_list = [checkpoint]
 
-    print('Training started....')
+    print('Simulator Model Training started....')
     
     history = model.fit_generator(
-        train_gen,
-        steps_per_epoch=len(d_train)//BATCH_SIZE,
-        epochs=EPOCHS,
-        validation_data=validation_gen,
-        validation_steps=len(d_valid)//BATCH_SIZE,
-        verbose=1,
-        callbacks=callbacks_list
+        train_gen, steps_per_epoch=len(x_train)//BATCH_SIZE,
+        epochs=EPOCHS, validation_data=validation_gen,
+        validation_steps=len(x_valid)//BATCH_SIZE,
+        verbose=1, callbacks=callbacks_list
     )
-
-    # print("Saving model..")
-    # model.save("./tl_classifier_keras.h5")
-    # print("Model Saved successfully!!")
 
     # Destroying the current TF graph to avoid clutter from old models / layers
     K.clear_session()
--- site_model_train.py
+++ site_model_train.py
@@ -1,10 +1,12 @@
 import os
+import glob
+import csv
+
+import random
 import numpy as np
 import pandas as pd
-import random
 import cv2
-import csv
-import glob
+
 from sklearn import model_selection
 
 from keras import backend as K
@@ -20,11 +22,11 @@ BATCH_SIZE = 4
 EPOCHS = 20
 NUM_CLASSES = 3
 
-IMAGE_HEIGHT = 600
-IMAGE_WIDTH = 800
-IMAGE_CHANNEL = 3
+IMG_H = 600
+IMG_W = 800
+IMG_CHN = 3
 
-BOTTOM_CROP = 178
+BOTTOM_CROP = 180  # seems better
 
 MODEL_FILE_NAME = './site_model.h5'
 
@@ -32,75 +34,67 @@ MODEL_FILE_NAME = './site_model.h5'
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
-if not os.path.exists('./site_train.csv'):
-    print('Please prepare ./site_train.csv by running prepare_csvs.py')
-else:
-    print('CSV already present')
 
-
+# Handy Augment function to apply random brightness to make model robust
 def random_brightness(image):
     # Convert 2 HSV colorspace from RGB colorspace
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # Generate new random brightness
-    rand = random.uniform(0.3, 1.0)
+    rand = random.uniform(0.4, 1.0) # komer changed 
     hsv[:, :, 2] = rand*hsv[:, :, 2]
     # Convert back to RGB colorspace
     new_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return new_img
 
-
+# Handy Augment function to zoom
 def zoom(image):
     zoom_pix = random.randint(0, 10)
-    zoom_factor = 1 + (2*zoom_pix)/IMAGE_HEIGHT
-    image = cv2.resize(image, None, fx=zoom_factor,
-                       fy=zoom_factor, interpolation=cv2.INTER_LINEAR)
-    top_crop = (image.shape[0] - IMAGE_HEIGHT)//2
-#     bottom_crop = image.shape[0] - top_crop - IMAGE_HEIGHT
-    left_crop = (image.shape[1] - IMAGE_WIDTH)//2
-#     right_crop = image.shape[1] - left_crop - IMAGE_WIDTH
-    image = image[top_crop: top_crop+IMAGE_HEIGHT,
-                  left_crop: left_crop+IMAGE_WIDTH]
-    return image
+    zoom_factor = 1 + (2*zoom_pix)/IMG_H
+    image = cv2.resize(image, None, fx=zoom_factor,fy=zoom_factor, interpolation=cv2.INTER_LINEAR)
+    top_crop = (image.shape[0] - IMG_H)//2
+    left_crop = (image.shape[1] - IMG_W)//2
+    new_img = image[top_crop: top_crop+IMG_H,
+                  left_crop: left_crop+IMG_W]
+    return new_img
 
 
 # fuction to read image from file
-def get_image(index, data, should_augment):
+def get_image(index, data, apply_augment=False):
     # Read image and appropiately traffic light color
     image = cv2.imread(os.path.join(
         ROOT_PATH, data['path'].values[index].strip()))
     color = data['class'].values[index]
-    lucky = random.randint(0, 1)
-    too_lucky = random.randint(0, 1)
-    unlucky = random.randint(0, 1)
-    if should_augment:
-        if lucky == 1:
+
+    if apply_augment:
+        apply_brightness = random.randint(0, 1)
+        apply_flip = random.randint(0, 1)
+        apply_zoom = random.randint(0, 1)
+        
+        if apply_brightness == 1:
             image = random_brightness(image)
-        if too_lucky == 1:
+        if apply_flip == 1:
             image = cv2.flip(image, 1)
-        if not unlucky == 1:
+        if apply_zoom == 1:
             image = zoom(image)
 
     return [image, color]
 
 # generator function to return images batchwise
-
-
-def generator(data, should_augment=False):
+def generator(data_set, apply_augment=False):
     while True:
-        # Randomize the indices to make an array
-        indices_arr = np.random.permutation(data.count()[0])
-        for batch in range(0, len(indices_arr), BATCH_SIZE):
+        # Shuffle the data set
+        shuffled_indices = np.random.permutation(data_set.count()[0])
+        for batch in range(0, len(shuffled_indices), BATCH_SIZE):
             # slice out the current batch according to batch-size
-            current_batch = indices_arr[batch:(batch + BATCH_SIZE)]
+            current_batch = shuffled_indices[batch:(batch + BATCH_SIZE)]
 
             # initializing the arrays, x_train and y_train
-            x_train = np.empty(
-                [0, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL], dtype=np.float32)
+            x_train = np.empty([0, IMG_H, IMG_W, IMG_CHN], dtype=np.float32)
             y_train = np.empty([0], dtype=np.int32)
 
             for i in current_batch:
                 # get an image and its corresponding color for an traffic light
-                [image, color] = get_image(i, data, should_augment)
+                [image, color] = get_image(i, data_set, apply_augment)
 
                 # Appending them to existing batch
                 x_train = np.append(x_train, [image], axis=0)
@@ -110,28 +104,41 @@ def generator(data, should_augment=False):
             yield (x_train, y_train)
 
 
-def get_model(time_len=1):
+
+# Define the model
+def construct_model():
 
     model = Sequential()
+    
+    #Define Input layer
+    #Add bottom cropping until bottom 180 the car bonnet is visible, mostly
     model.add(Cropping2D(cropping=((0, BOTTOM_CROP), (0, 0)),
-                         input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL)))
+                         input_shape=(IMG_H, IMG_W, IMG_CHN)))
+    #Normalize the pixel values
     model.add(Lambda(lambda x: x/127.5 - 1.))
+    
+    #Define First Layer
     model.add(Conv2D(32, 8, strides=(4, 4), padding="same", activation='relu'))
+    #Follow with Max Pooling Layer
     model.add(MaxPooling2D(2, 2))
-    model.add(Conv2D(32, 4, strides=(2, 2), padding="same", activation='relu'))
+    
+    # Define second layer & a max pooling follow-up layer
+    model.add(Conv2D(64, 4, strides=(2, 2), padding="same", activation='relu'))
     model.add(MaxPooling2D(2, 2))
-#     model.add(Conv2D(64, 5, strides=(2, 2), padding="same", activation='relu'))
+    
+    #Time to flatten add dropout to minimize overfitting
     model.add(Flatten())
-    model.add(Dropout(.35))
+    model.add(Dropout(.4))
+    
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(.5))
+    
     model.add(Dense(NUM_CLASSES))
-    # model.add(Lambda(lambda x: (K.exp(x) + 1e-4) / (K.sum(K.exp(x)) + 1e-4)))
+ 
     model.add(Lambda(lambda x: K.tf.nn.softmax(x)))
     
-
-    model.compile(optimizer=Adam(lr=5e-4),
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+    # Think about learning rate later, Use Adam 
+    model.compile(optimizer=Adam(lr=5e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
     model.summary()
 
@@ -140,30 +147,33 @@ def get_model(time_len=1):
 
 if __name__ == "__main__":
 
-    data = pd.read_csv(os.path.join('./site_train.csv'))
+    # check if csv file is available, if not inform to prepare one.
+    if not os.path.exists('./site_train.csv'):
+        print('Please prepare ./site_train.csv by running prepare_csvs.py')
+        return
+    
+    print('CSV already present')
+    data_set = pd.read_csv(os.path.join('./site_train.csv'))
 
-    # Split data into random training and validation sets
-    d_train, d_valid = model_selection.train_test_split(data, test_size=.2)
+    # Split data set into random training and validation sets
+    
+    x_train, x_valid = model_selection.train_test_split(data_set, test_size=.2)
 
-    train_gen = generator(d_train, True)
-    validation_gen = generator(d_valid, False)
+    train_gen = generator(x_train, True)
+    validation_gen = generator(x_valid, False)
 
-    model = get_model()
+    model = construct_model()
 
     # checkpoint to save best weights after each epoch based on the improvement in val_loss
     checkpoint = ModelCheckpoint(MODEL_FILE_NAME, monitor='val_loss', verbose=1,save_best_only=True, mode='min',save_weights_only=False)
-    callbacks_list = [checkpoint] #,callback_each_epoch]
+    callbacks_list = [checkpoint]
 
-    print('Training started....')
+    print('Site Model Training started. ')
     
     history = model.fit_generator(
-        train_gen,
-        steps_per_epoch=len(d_train)//BATCH_SIZE,
-        epochs=EPOCHS,
-        validation_data=validation_gen,
-        validation_steps=len(d_valid)//BATCH_SIZE,
-        verbose=1,
-        callbacks=callbacks_list
+        train_gen, steps_per_epoch=len(x_train)//BATCH_SIZE, epochs=EPOCHS,
+        validation_data=validation_gen, validation_steps=len(x_valid)//BATCH_SIZE,
+        verbose=1, callbacks=callbacks_list
     )
 
     K.clear_session()
